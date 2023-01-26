4927293640819b3846071f27ac5560590c2f6f3e
Mehulparekh144 <mehulparekh144@gmail.com>
2023-01-26 17:46:34 +0530

    Updates

---
+++ precache-manifest.ac76291f9a69094fe6ac94b72ce64305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c14452502864fe76370a",
+    "url": "/Text-Utility-React/static/css/main.eef3b916.chunk.css"
+  },
+  {
+    "revision": "c14452502864fe76370a",
+    "url": "/Text-Utility-React/static/js/main.c1445250.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Text-Utility-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text-Utility-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "a0a8bdd886d55e70fd89",
+    "url": "/Text-Utility-React/static/js/runtime~main.a0a8bdd8.js"
+  },
+  {
+    "revision": "a40d6c3fc12abf5b371dd487d144e021",
+    "url": "/Text-Utility-React/index.html"
+  }
+];
